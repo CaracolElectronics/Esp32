2a18503063e657463a005ffef8dad10c106c8033
CaracolElectronics <59808093+CaracolElectronics@users.noreply.github.com>
2020-01-12 16:33:45 -0600

    Add files via upload

---
+++ Esp32_PWM.ino
@@ -0,0 +1,86 @@
+///////////////////////////
+/// Caracol Electronics ///
+///                     ///
+/// Ejemplo PWM con ESP32//
+///////////////////////////
+
+const int ledGPIO = 18; // cualquier GPIO
+const int ledGPIO1 = 16;
+const int ledGPIO2 = 17;
+
+//propiedades señal PWM//////////
+const int canalPWM = 0;
+const int canalPWM1 = 1;
+const int canalPWM2 = 2;// 0-15
+
+const int frecuencia = 10000; // en Hz
+const int resolucion = 8; // 0-255
+/////////////////////////////////
+
+void setup() 
+{
+  // 1) configura señal PWM
+  ledcSetup(canalPWM, frecuencia, resolucion);
+  ledcSetup(canalPWM1, frecuencia, resolucion);
+  ledcSetup(canalPWM2, frecuencia, resolucion);
+
+  // 2) asigna la señal creada al GPIO de su elección 
+  ledcAttachPin(ledGPIO, canalPWM);
+  ledcAttachPin(ledGPIO1, canalPWM1);
+  ledcAttachPin(ledGPIO2, canalPWM2);
+
+}
+
+void loop() 
+{
+  // prendiendo
+  int CT = 0; //ciclo de trabajo
+  while(CT < 255)
+  {
+    ledcWrite(canalPWM, CT);
+    delay(5);
+    CT++;
+  }
+  CT = 0;
+  while(CT < 255)
+  {
+    ledcWrite(canalPWM1, CT);
+    delay(5);
+    CT++;
+  }
+  CT = 0;
+  while(CT < 255)
+  {
+    ledcWrite(canalPWM2, CT);
+    delay(5);
+    CT++;
+  }
+  
+  
+  
+  
+  /// apagando ! 
+  CT = 255; //ciclo de trabajo
+  while(CT > 0)
+  {
+    ledcWrite(canalPWM, CT);
+    delay(5);
+    CT--;
+  }
+  CT = 255;
+  while(CT > 0)
+  {
+    ledcWrite(canalPWM1, CT);
+    delay(5);
+    CT--;
+  }
+  CT = 255;
+  while(CT > 0)
+  {
+    ledcWrite(canalPWM2, CT);
+    delay(5);
+    CT--;
+  }
+
+  CT=0;
+}
